0b0db6e65c81fd18c2fc5b2d85b4695c2e5a56bd
Shreyanshu shankar <shreyanshu7101904@gmail.com>
2018-07-28 23:17:00 +0530

    Update email_config.py

--- email_config.py
+++ email_config.py
@@ -5,14 +5,14 @@ from configruation import config,userId,Password
 
 
 def add_data(value):
-'''Function to insert values in diffrent node from existing node users'''
+	'''Function to insert values in diffrent node from existing node users'''
 	firebase1 = pyrebase.initialize_app(config)
 	db1 =firebase1.database()
 	db1.child("userInformation").push(value)
 
 
 def send_email_to(username,input_id):
-'''function to send email to user'''
+	'''function to send email to user'''
 	s = smtplib.SMTP('smtp.gmail.com', 587)
 	s.starttls()
 	s.login(userId, Password)
